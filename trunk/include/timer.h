3196aead0ffa10156a17065197d4594972dd2b2b
meemo <meemo@f4e652a2-3e31-0410-bc75-1f482bb99a99>
2007-01-04 22:48:01 +0000

    Changed the timer interval to 5 ms.

--- trunk/include/timer.h
+++ trunk/include/timer.h
@@ -7,7 +7,9 @@ extern "C" {
 
 extern volatile unsigned int timer_ticks;
 
-#define TICKS_PER_SEC 10
+// 100 hz = 10 ms, so 20 hz is 50ms, and 10 hz = 100ms, 50 hz = 20ms , 200 hz = 5ms
+#define TICKS_PER_SEC 200
+// 1000ms / desired_ms = freq
 
 void timer_phase(int hz);
 void timer_install();

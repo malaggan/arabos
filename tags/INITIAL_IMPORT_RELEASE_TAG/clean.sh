bfec3d584bf1a111c0deeca6d7024d115580079e
mohammad.nabil.h <mohammad.nabil.h@f4e652a2-3e31-0410-bc75-1f482bb99a99>
2007-05-28 01:33:00 +0000

    Removing undeeded tag

--- tags/INITIAL_IMPORT_RELEASE_TAG/clean.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#@echo off
-#make clean
-#pause
-make clean
-#pause
--- tags/INITIAL_IMPORT_RELEASE_TAG/i386/idt_s.asm
+++
@@ -1,8 +0,0 @@
-; Loads the IDT defined in '_idtp' into the processor.
-; This is declared in C as 'extern void idt_load();'
-extern idt_ptr
-global idt_load
-SECTION .text
-idt_load:
-	lidt [idt_ptr]
-	ret

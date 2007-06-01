e905aefbb33a25d3803c5df9095db98500d2f472
mohammad.nabil.h <mohammad.nabil.h@f4e652a2-3e31-0410-bc75-1f482bb99a99>
2007-06-01 22:55:06 +0000

    Added runtime support for constructing static objectst ( not yet destructin ) and pure virtual functions. The latter is not tested yet.

--- init/cpptest.cpp
+++ init/cpptest.cpp
@@ -1,5 +1,7 @@
 #include <cpptest.h>
 
+CPPEmt7an myStaticObject[3];
+
 CPPTest::CPPTest() 
 { 
     printf("in CPPTest constructor\n");
@@ -7,7 +9,7 @@ CPPTest::CPPTest()
 
 void CPPTest::Test()
 {
-    printf("in CPPTest\n");
+    printf("in CPPTest test\n");
 }
 
 CPPEmt7an::CPPEmt7an()
@@ -17,14 +19,19 @@ CPPEmt7an::CPPEmt7an()
 
 void CPPEmt7an::Test()
 {
-    printf("in CPPEmt7an");
+    printf("in CPPEmt7an test");
 }
+
+extern "C" void call_ctors();
 
 extern "C"
 void cppmain()
-{
-    CPPTest *test;
-    CPPEmt7an emt7an;
-    test = &emt7an;
-    test->Test();
+{    
+    printf("now in cppmain\n");
+    call_ctors();
+    //CPPTest *test;
+    //CPPEmt7an emt7an;
+    //test = &emt7an;
+    //test->Test();
+    myStaticObject[0].Test();
 }

c85241a2994ae82f13107a4d1dc1f02eba51bf3d
mohammad.nabil.h <mohammad.nabil.h@f4e652a2-3e31-0410-bc75-1f482bb99a99>
2009-05-09 21:02:34 +0000

    Added semaphore implementation: Hooray ...  : D :D

---
+++ include/semaphore.h
@@ -0,0 +1,24 @@
+/* 
+ * File:   semaphore.h
+ * Author: meemo
+ *
+ * Created on May 9, 2009, 11:27 PM
+ */
+
+#ifndef _SEMAPHORE_H
+#define	_SEMAPHORE_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+    typedef int semaphore_t;
+    void wait(volatile semaphore_t* sem);
+    void signal(volatile semaphore_t* sem);
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* _SEMAPHORE_H */
+

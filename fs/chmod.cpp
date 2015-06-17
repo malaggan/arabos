72afcacc3327217ee5c69797c70f9341e6e6be70
Mohammad Alaggan <mohammad.nabil.h@gmail.com>
2015-06-17 17:38:23 +0200

    Filesystem, initial (does not compile)

---
+++ fs/chmod.cpp
@@ -0,0 +1,16 @@
+#include "file_t.hpp"
+#include <fuse.h>
+#include <syslog.h>
+
+int sfs_chmod (const char *path, mode_t mode)
+{
+    int  index_file=ROOT.find(path);
+    syslog(LOG_DEBUG,"In chmod \n");
+    if(index_file==-1)
+				return -EEXIST;
+
+    if(!hd.blocks[hd.blocks[index_file].get<file_t>().inode].get<inode_t>().check_permission(fuse_get_context()->uid, fuse_get_context()->gid))
+				return -EACCES;
+    hd.blocks[index_file].get<file_t>().mode=mode;
+    return 0;
+}

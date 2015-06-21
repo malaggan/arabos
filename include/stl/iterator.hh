a5dcd80003b85c4ea34a667e7891ceeb4fb0cdec
Mohammad Alaggan <mohammad.nabil.h@gmail.com>
2015-06-22 00:05:00 +0200

    uncommitted changes found on laptop, but probably are already seen on pc

--- include/stl/iterator.hh
+++ include/stl/iterator.hh
@@ -1,50 +1,243 @@
+// -*- mode: c++; -*-
+
 #pragma once
+
+#include <types.h> // for ptrdiff_t and move
+
 namespace aos {
-    template<typename T, typename Distance = ptrdiff_t,
-	     typename Pointer = T*, typename Reference = T&>
+    struct input_iterator_tag { };
+    struct output_iterator_tag { };
+    struct forward_iterator_tag : public input_iterator_tag { };
+    struct bidirectional_iterator_tag : public forward_iterator_tag { };
+    struct random_access_iterator_tag : public bidirectional_iterator_tag { };
+
+    template<typename Category, typename T, typename Distance = ptrdiff_t, typename Pointer = T*, typename Reference = T&>
     struct iterator
     {
-      typedef T        value_type;
-      typedef Distance  difference_type;
-      typedef Pointer   pointer;
-      typedef Reference reference;
+	using  iterator_category = Category 	;
+	using  value_type	 = T        	;
+	using  difference_type	 = Distance 	;
+	using  pointer		 = Pointer  	;
+	using  reference	 = Reference	;
     };
+    // N3844 (for explanation), and N3923 for most recent wording
+    template<typename...> using void_t = void;
     
-    // std::chrono::system_clock::now()
-    // std::begin
-    template< class C >
-    auto begin( C& c ) { return c.begin(); }
+    template<typename It, typename = void_t<>>
+	struct __iterator_traits { };
+    
+    template<typename It>
+    struct __iterator_traits<_Iterator, void_t<typename It::iterator_category,
+					       typename It::value_type,
+					       typename It::difference_type,
+					       typename It::pointer,
+					       typename It::reference>>
+    {
+        using iterator_category = typename It::iterator_category;
+        using value_type        = typename It::value_type;
+        using difference_type   = typename It::difference_type;
+        using pointer           = typename It::pointer;
+        using reference         = typename It::reference;
+    };
 
-    template< class C >
-    auto begin( const C& c ) { return c.begin(); }
+    template<typename It>
+    struct iterator_traits : public __iterator_traits<_Iterator> { };
 
-    template< class T, size_t N >
-    constexpr T* begin( T (&array)[N] ) { return array; }
 
-    template< class C >
-    constexpr auto cbegin( const C& c ) { return c.begin(); }
-    // std::end
-    template< class C >
-    auto end( C& c ) { return c.end(); }
+    template<typename T>
+    struct iterator_traits<T*>
+    {
+        using iterator_category = random_access_iterator_tag;
+        using value_type        = T;
+        using difference_type   = ptrdiff_t;
+        using pointer           = T*;
+        using reference         = T&;
+    };
 
-    template< class C >
-    auto end( const C& c ) { return c.end(); }
 
-    template< class T, size_t N >
-    constexpr T* end( T (&array)[N] ) { return array+N; }
+    template<typename T>
+    struct iterator_traits<T const *>
+    {
+        using iterator_category = random_access_iterator_tag;
+        using value_type        = T;
+        using difference_type   = ptrdiff_t;
+        using pointer           = T const *;
+        using reference         = T const &;
+    };
 
-    template< class C >
-    constexpr auto cend( const C& c ) { return c.end(); }
+    
+    template<class C> auto begin			(C &c)		{ return c.begin(); }
+    template<class C> auto end				(C &c)		{ return c.end(); }
+    template<class C> auto begin			(C const &c)	{ return c.begin(); }
+    template<class C> auto end				(C const &c)	{ return c.end(); }
+    template<class T, size_t N> constexpr T* begin	(T (&array)[N])	{ return array; }
+    template<class T, size_t N> constexpr T* end	(T (&array)[N])	{ return array+N; }
+    template<class C> auto cbegin			(C const &c)	{ return c.begin(); }
+    template<class C> auto cend				(C const &c)	{ return c.end(); }
 
-    // std::distance
-    template< class InputIt >
-    size_t // Note:: should be "typename std::iterator_traits<InputIt>::difference_type"
-    distance( InputIt first, InputIt last ) {
-	size_t dist = 0;
-	while(first++!=last) dist++;
+    // not std c++, but helpful:
+    template <typname It> using iterator_category_t	= typename iterator_category<It>::iterator_category;
+    template <typname It> using value_type_t		= typename iterator_category<It>::value_type;
+    template <typname It> using difference_type_t	= typename iterator_category<It>::difference_type;   
+    template <typname It> using pointer_t		= typename iterator_category<It>::pointer;
+    template <typname It> using reference_t		= typename iterator_category<It>::reference;
+
+    template<class InputIt>
+    difference_type_t<InputIt>
+    distance(InputIt first, InputIt last) {
+	difference_type_t<InputIt> dist = 0;
+	while(first++ != last) dist++;
 	return dist;
     }
+
+    // TODO: distance can be made more efficient for random_access_iterators
+    // TODO: advance()
+
+
+    // back_insert_iterator is an OutputIterator that appends to a container for which it was constructed. The container's push_back() member function is called whenever the iterator (whether dereferenced or not) is assigned to. Incrementing the back_insert_iterator is a no-op.
+    template<class Container>
+    class back_insert_iterator : public iterator<output_iterator_tag, void, void, void, void>
+    {
+	Container &member;
+    public:
+	explicit back_insert_iterator(Container &c) : member(c) {}
+	back_insert_iterator<Container>& operator*() { return *this; } // no-op
+	back_insert_iterator<Container>& operator++() { return *this; } // no-op
+	back_insert_iterator<Container>  operator++(int) { return *this; } // no-op
+	back_insert_iterator<Container>&
+	operator=(const value_type_t<Container> &value) { member.push_back(value); return *this; }
+	back_insert_iterator<Container>&
+	operator=(value_type_t<Container> &&value ) { member.push_back(move(value)); return *this; }
+    };
+
+    template<class Container> back_insert_iterator<Container> back_inserter(Container &c)
+    {
+	return back_insert_iterator<Container>(c);
+    }
+
+    template<typename It>
+    class reverse_iterator : public iterator<iterator_category_t<It>,
+					     value_type_t<It>,
+					     difference_type_t<It>,
+					     pointer_t<It>,
+					     reference_t<It> >
+    {
+    private:
+	It current;
+
+    public:
+        using iterator_type = It;
+
+	reverse_iterator() : current{} { }
+
+	explicit reverse_iterator(iterator_type x) : current(x) { }
+
+	reverse_iterator(const reverse_iterator& x) = default;
+
+	template<typename Iter> reverse_iterator(const reverse_iterator<Iter>& x) : current(x.base()) { }
+
+	iterator_type base() const { return current; }
+
+	reference operator*() const {
+	    It tmp = current;
+	    return *--tmp;
+	}
+
+	pointer operator->() const { return &(operator*()); }
+
+	reverse_iterator& operator++() {
+		--current;
+		return *this;
+	    }
+
+	reverse_iterator operator++(int) {
+		reverse_iterator tmp = *this;
+		--current;
+		return tmp;
+	    }
+
+	reverse_iterator& operator--() {
+		++current;
+		return *this;
+	    }
+
+	reverse_iterator operator--(int) {
+		reverse_iterator tmp = *this;
+		++current;
+		return tmp;
+	    }
+
+	reverse_iterator operator+(difference_type n) const { return reverse_iterator(current - n); }
+
+	reverse_iterator& operator+=(difference_type n) {
+		current -= n;
+		return *this;
+	    }
+
+	reverse_iterator operator-(difference_type n) const { return reverse_iterator(current + n); }
+
+	reverse_iterator& operator-=(difference_type n) {
+		current += n;
+		return *this;
+	    }
+
+	reference operator[](difference_type n) const { return *(*this + n); }
+    };
+
+
+    template<typename ItL, typename ItR>
+    inline bool
+    operator==(const reverse_iterator<ItL>& x,
+	       const reverse_iterator<ItR>& y)
+    { return x.base() == y.base(); }
+
+    template<typename ItL, typename ItR>
+    inline bool
+    operator<(const reverse_iterator<ItL>& x,
+	      const reverse_iterator<ItR>& y)
+    { return y.base() < x.base(); }
+
+    template<typename ItL, typename ItR>
+    inline bool
+    operator!=(const reverse_iterator<ItL>& x,
+	       const reverse_iterator<ItR>& y)
+    { return !(x == y); }
+
+    template<typename ItL, typename ItR>
+    inline bool
+    operator>(const reverse_iterator<ItL>& x,
+	      const reverse_iterator<ItR>& y)
+    { return y < x; }
+
+    template<typename ItL, typename ItR>
+    inline bool
+    operator<=(const reverse_iterator<ItL>& x,
+	       const reverse_iterator<ItR>& y)
+    { return !(y < x); }
+
+    template<typename ItL, typename ItR>
+    inline bool
+    operator>=(const reverse_iterator<ItL>& x,
+	       const reverse_iterator<ItR>& y)
+    { return !(x < y); }
+
+    template<typename ItL, typename ItR>
+    inline auto
+    operator-(const reverse_iterator<ItL>& x,
+	      const reverse_iterator<ItR>& y)
+    { return y.base() - x.base(); }
+
+    template<typename It>
+    inline reverse_iterator<It>
+    make_reverse_iterator(It i)
+    { return reverse_iterator<It>(i); }
+
+
+
+    
+    
     // std::ceil
+    // std::chrono::system_clock::now()
     int ceil(double); // TODO: implement
     // std::chrono::system_clock::to_time_t(hd.blocks[hd.blocks[file_inode].get<file_t>().inode].get<inode_t>().ctime);
     // std::make_shared
@@ -54,39 +247,24 @@ namespace aos {
 #include <bits/copy_if.hh>
 #include <bits/copy_n.hh>
 #include <bits/find.hh>
-    
-    // std::move
-    template<typename _Tp>
-    constexpr remove_reference_t<_Tp>&&
-    move(_Tp&& __t) noexcept
-    { return static_cast<remove_reference_t<_Tp>&&>(__t); }
-    
-    // std::back_inserter
-    // back_insert_iterator is an OutputIterator that appends to a container for which it was constructed. The container's push_back() member function is called whenever the iterator (whether dereferenced or not) is assigned to. Incrementing the std::back_insert_iterator is a no-op.
-    template< class Container >
-    class back_insert_iterator
-    {
-	using value_type = void;
-	using difference_type =	void;
-	using pointer = void;
-	using reference = void;
-	Container& member;
-    public:
-	explicit back_insert_iterator(Container& c) : member(c) {}
-	back_insert_iterator<Container>& operator*() { return *this; } // no-op
-	back_insert_iterator<Container>& operator++() { return *this; } // no-op
-	back_insert_iterator<Container>  operator++( int ) { return *this; } // no-op
-	back_insert_iterator<Container>&
-	operator=( const typename Container::value_type& value ) { member.push_back(value); return *this; }
-	back_insert_iterator<Container>&
-	operator=( typename Container::value_type&& value ) { member.push_back(move(value)); return *this; }
-    };
-    template< class Container >
-    back_insert_iterator<Container> back_inserter( Container& c )
-    {
-	return back_insert_iterator<Container>(c);
-    }
 
+    
+    template<typename value_type,
+	     typename Iterator, typename const_iterator, typename reverse_iterator, typename 
+	     typename Size_type = size_t,
+	     typename Pointer = value_type*,
+	     typename Reference = value_type&,
+	     typename Const_reference = value_type const &>
+    struct container
+    {
+	using  iterator_category = Category 	;
+	using  value_type	 = T        	;
+	using  difference_type	 = Distance 	;
+	using  pointer		 = Pointer  	;
+	using  reference	 = Reference	;
+    };
+
+    
     template<typename T, size_t N>
     class static_vector {
 	T a[N];
--- include/types.h
+++ include/types.h
@@ -2,89 +2,81 @@
 #ifndef TYPES_H 
 #define TYPES_H 
 
-typedef __INT8_TYPE__ int8_t;
-typedef __INT16_TYPE__ int16_t;
-typedef __INT32_TYPE__ int32_t;
-typedef __INT64_TYPE__ int64_t;
+using int8_t = __INT8_TYPE__;
+using int16_t = __INT16_TYPE__;
+using int32_t = __INT32_TYPE__;
+using int64_t = __INT64_TYPE__;
 
-typedef __INT_FAST8_TYPE__ int_fast8_t;
-typedef __INT_FAST16_TYPE__ int_fast16_t;
-typedef __INT_FAST32_TYPE__ int_fast32_t;
-typedef __INT_FAST64_TYPE__ int_fast64_t;
+using int_fast8_t = __INT_FAST8_TYPE__;
+using int_fast16_t = __INT_FAST16_TYPE__;
+using int_fast32_t = __INT_FAST32_TYPE__;
+using int_fast64_t = __INT_FAST64_TYPE__;
 
-typedef __INT_LEAST8_TYPE__ int_least8_t;
-typedef __INT_LEAST16_TYPE__ int_least16_t;
-typedef __INT_LEAST32_TYPE__ int_least32_t;
-typedef __INT_LEAST64_TYPE__ int_least64_t;
+using int_least8_t = __INT_LEAST8_TYPE__;
+using int_least16_t = __INT_LEAST16_TYPE__;
+using int_least32_t = __INT_LEAST32_TYPE__;
+using int_least64_t = __INT_LEAST64_TYPE__;
 
-typedef __INTMAX_TYPE__ intmax_t;
-typedef __INTPTR_TYPE__ intptr_t;
+using intmax_t = __INTMAX_TYPE__;
+using intptr_t = __INTPTR_TYPE__;
 
-typedef __UINT8_TYPE__ uint8_t;
-typedef __UINT16_TYPE__ uint16_t;
-typedef __UINT32_TYPE__ uint32_t;
-typedef __UINT64_TYPE__ uint64_t;
+using uint8_t = __UINT8_TYPE__;
+using uint16_t = __UINT16_TYPE__;
+using uint32_t = __UINT32_TYPE__;
+using uint64_t = __UINT64_TYPE__;
 
-typedef __UINT_FAST8_TYPE__ uint_fast8_t;
-typedef __UINT_FAST16_TYPE__ uint_fast16_t;
-typedef __UINT_FAST32_TYPE__ uint_fast32_t;
-typedef __UINT_FAST64_TYPE__ uint_fast64_t;
+using uint_fast8_t = __UINT_FAST8_TYPE__;
+using uint_fast16_t = __UINT_FAST16_TYPE__;
+using uint_fast32_t = __UINT_FAST32_TYPE__;
+using uint_fast64_t = __UINT_FAST64_TYPE__;
 
-typedef __UINT_LEAST8_TYPE__ uint_least8_t;
-typedef __UINT_LEAST16_TYPE__ uint_least16_t;
-typedef __UINT_LEAST32_TYPE__ uint_least32_t;
-typedef __UINT_LEAST64_TYPE__ uint_least64_t;
+using uint_least8_t = __UINT_LEAST8_TYPE__;
+using uint_least16_t = __UINT_LEAST16_TYPE__;
+using uint_least32_t = __UINT_LEAST32_TYPE__;
+using uint_least64_t = __UINT_LEAST64_TYPE__;
 
-typedef __UINTMAX_TYPE__ uintmax_t;
-typedef __UINTPTR_TYPE__ uintptr_t;
+using uintmax_t = __UINTMAX_TYPE__;
+using uintptr_t = __UINTPTR_TYPE__;
 
-typedef __SIZE_TYPE__ size_t;
-typedef __PTRDIFF_TYPE__ ptrdiff_t;
+using size_t = __SIZE_TYPE__;
+using ptrdiff_t = __PTRDIFF_TYPE__;
 
-typedef uint32_t phys_addr_t;
+using phys_addr_t = uint32_t;
 
-typedef uint32_t uid_t;
-typedef uint32_t gid_t;
-typedef uint32_t mode_t;
-typedef int64_t  __time_t;
+using uid_t = uint32_t;
+using gid_t = uint32_t;
+using mode_t = uint32_t;
+using __time_t =int64_t;
 
 #ifdef __cplusplus
 namespace aos {  
     template<class T, T v>
     struct integral_constant {
 	static constexpr T value = v;
-	typedef T value_type;
-	typedef integral_constant type;
+	using value_type = T;
+	using type = integral_constant;
 	constexpr operator value_type() const noexcept { return value; }
 	constexpr value_type operator()() const noexcept { return value; } 
     };
     typedef integral_constant<bool,true> true_type;
     typedef integral_constant<bool,false> false_type;
-    template<bool B, class T = void>
-    struct enable_if {};
- 
-    template<class T>
-    struct enable_if<true, T> { typedef T type; };
-    template< bool B, class T = void >
-    using enable_if_t = typename enable_if<B,T>::type;
-    template<class T, class U>
-    struct is_same : false_type {}; 
-    template<class T>
-    struct is_same<T, T> : true_type {};
+    template<bool B, class T = void> struct enable_if {}; 
+    template<class T> struct enable_if<true, T> { using type = T; };
+    template<bool B, class T = void> using enable_if_t = typename enable_if<B,T>::type;
+    template<class T, class U> struct is_same : false_type {}; 
+    template<class T>          struct is_same<T, T> : true_type {};
 
-    template< class T > struct remove_reference      {typedef T type;};
-    template< class T > struct remove_reference<T&>  {typedef T type;};
-    template< class T > struct remove_reference<T&&> {typedef T type;};
-    template< class T > using remove_reference_t = typename remove_reference<T>::type;
+    template<class T> struct remove_reference      {using type = T;};
+    template<class T> struct remove_reference<T&>  {using type = T;};
+    template<class T> struct remove_reference<T&&> {using type = T;};
+    template<class T> using remove_reference_t = typename remove_reference<T>::type;
     
-    template<typename _Tp> constexpr _Tp&&
-    forward(remove_reference_t<_Tp>& __t) noexcept
-    { return static_cast<_Tp&&>(__t); }
-    
-    template<typename _Tp> constexpr _Tp&&
-    forward(remove_reference_t<_Tp>&& __t) noexcept
-    { return static_cast<_Tp&&>(__t); }
-    
+    template<typename T> constexpr T&& forward(remove_reference_t<T>& t) noexcept { return static_cast<T&&>(t); }    
+    template<typename T> constexpr T&& forward(remove_reference_t<T>&& t) noexcept { return static_cast<T&&>(t); }
+
+
+    template<typename T> constexpr remove_reference_t<T>&& move(T&& t) noexcept {return static_cast<remove_reference_t<T>&&>(t);}
+
 }
 #endif
 

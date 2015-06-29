28682b826e79b458fecaf9a1e34d89bd126357f0
Mohammad Alaggan <mohammad.nabil.h@gmail.com>
2015-06-29 11:53:08 +0200

    fixed keyboard problem. no irq7's are dopped.

--- include/timer.h
+++ include/timer.h
@@ -19,7 +19,7 @@ extern volatile unsigned int scheduling_started;
 int schedule(struct interrupt_frame *r);
 
 // 100 hz = 10 ms, so 20 hz is 50ms, and 10 hz = 100ms, 50 hz = 20ms , 200 hz = 5ms
-#define TICKS_PER_SEC 2000
+#define TICKS_PER_SEC 10
 // 1000ms / desired_ms = freq
 
 void timer_phase(int hz);
